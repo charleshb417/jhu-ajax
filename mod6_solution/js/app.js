6fb813c9655964f1060d53b9f5b520a4d1141a4d
Charles Bishop <charleshb417@gmail.com>
2017-02-20 17:24:34 -0500

    Implemented coursera styling requirements

--- mod6_solution/js/app.js
+++ mod6_solution/js/app.js
@@ -8,31 +8,31 @@
   function LunchCheckController($scope){
   	var HIGH_AMOUNT = 3;
 
+  	$scope.messageType = "";
   	$scope.userInputTextbox = "";
   	$scope.latestMessage = "Please list comma separated dishes that you usually have for lunch. NOTE: empty dishes and trailing commas will be ignored (i.e. dish1,,dish2 OR ,dish1 OR dish1,dish2,)";
   	
   	$scope.checkIfTooMuch = function(){
-  		if ($scope.userInputTextbox.length == 0){
-  		  $scope.latestMessage = "Please enter data first.";
-  		}
-  		else {
-   		  var lunchInputs = $scope.userInputTextbox.split(',');
-   		  var sanitizedInputs = [];
+		var lunchInputs = $scope.userInputTextbox.split(',');
+		var sanitizedInputs = [];
 
-   		  // Iterate through lunchInputs and remove the empty inputs
-   		  for (var i=0; i<lunchInputs.length; i++){
-   		  	if (lunchInputs[i].trim().length > 0){
-   		  	  sanitizedInputs.push(lunchInputs[i]);
-   		  	}
-   		  }
+		// Iterate through lunchInputs and remove the empty inputs
+		for (var i=0; i<lunchInputs.length; i++){
+		  if (lunchInputs[i].trim().length > 0){
+		    sanitizedInputs.push(lunchInputs[i]);
+		  }
+		}
 
-   		  if (sanitizedInputs.length <= HIGH_AMOUNT){
-   		  	$scope.latestMessage = "Enjoy!";
-   		  }
-   		  else {
-   		  	$scope.latestMessage = "Too much!";
-   		  }
-  		}
+		if (sanitizedInputs.length > 0 && sanitizedInputs.length <= HIGH_AMOUNT){
+	      $scope.latestMessage = "Enjoy!";
+		  $scope.messageType = "message-valid";
+		} else if (sanitizedInputs.length > HIGH_AMOUNT){
+		  $scope.latestMessage = "Too much!";
+	      $scope.messageType = "message-valid";
+		} else {
+		  $scope.latestMessage = "Please enter data first.";
+		  $scope.messageType = "message-invalid";
+		}
   	}
   }
 
