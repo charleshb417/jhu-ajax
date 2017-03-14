1e916b7543176f1ad8517ff328f8e5cd8d6b8828
Charles Bishop <charleshb417@gmail.com>
2017-03-14 10:29:30 -0400

    Service returns search results

--- mod8_solution/js/app.js
+++ mod8_solution/js/app.js
@@ -7,14 +7,33 @@
 
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController(MenuSearchService){
+    var self = this;
 
+    self.searchTerm = "";
+    self.narrowDown = function(){
+      MenuSearchService.getMatchedMenuItems(self.searchTerm);
+    };
   }
 
-  function MenuSearchService(){
+  MenuSearchService.$inject = ['$http'];
+  function MenuSearchService($http){
     var service = this;
 
     service.getMatchedMenuItems = function(searchTerm){
+      var serviceUrl = "https://davids-restaurant.herokuapp.com/menu_items.json";
+      var searchTermLowerCase = searchTerm.toLowerCase();
 
+      return $http({ method: 'GET', url: serviceUrl }).then(function (result) {
+        var foundItems = [];
+        for (var i=0; i<result.data.menu_items.length; i++){
+          var item = result.data.menu_items[i];
+
+          if (item.description.toLowerCase().indexOf(searchTerm) != -1){
+            foundItems.push(item);
+          }
+        }
+        return foundItems;
+      });
     };
   }
 
