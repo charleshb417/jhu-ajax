f8366d32f84686090134861ce9c43e7d765cbaf6
Charles Bishop <charleshb417@gmail.com>
2017-04-22 11:36:00 -0400

    Routing and stubs implemented

--- mod10_solution/src/restaurant.module.js
+++ mod10_solution/src/restaurant.module.js
@@ -7,11 +7,28 @@
 angular.module('restaurant', ['public'])
 .config(config);
 
-config.$inject = ['$urlRouterProvider'];
-function config($urlRouterProvider) {
+config.$inject = ['$stateProvider', '$urlRouterProvider'];
+function config($stateProvider, $urlRouterProvider) {
 
   // If user goes to a path that doesn't exist, redirect to public root
   $urlRouterProvider.otherwise('/');
+
+  // Set up UI states
+  $stateProvider
+    .state('home', {
+      url: '/'
+    })
+
+    .state('myinfo', {
+      url: '/myinfo',
+      templateUrl: 'src/public/myinfo/myinfo.html'
+    })
+
+    .state('signup', {
+      url: '/signup',
+      templateUrl: 'src/public/signup/signup.html'
+    });
+
 }
 
 })();
